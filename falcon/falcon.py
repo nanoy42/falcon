21022bb678091fda9d4dd8c7bc5c245939bf56d5
Yoann Piétri <me@nanoy.fr>
2023-01-31 08:40:32 +0100

    Create PublicKey from the public key

--- falcon/falcon.py
+++ falcon/falcon.py
@@ -178,13 +178,67 @@ class PublicKey:
     This class contains methods for performing public key operations in Falcon.
     """
 
-    def __init__(self, sk):
+    def __init__(self, n, h):
         """Initialize a public key."""
-        self.n = sk.n
-        self.h = sk.h
-        self.hash_to_point = sk.hash_to_point
-        self.signature_bound = sk.signature_bound
-        self.verify = sk.verify
+        self.n = n
+        self.h = h
+        self.signature_bound = Params[n]["sig_bound"]
+        self.sig_bytelen = Params[n]["sig_bytelen"]
+
+    def hash_to_point(self, message, salt):
+        """
+        Hash a message to a point in Z[x] mod(Phi, q).
+        Inspired by the Parse function from NewHope.
+        """
+        n = self.n
+        if q > (1 << 16):
+            raise ValueError("The modulus is too large")
+
+        k = (1 << 16) // q
+        # Create a SHAKE object and hash the salt and message.
+        shake = SHAKE256.new()
+        shake.update(salt)
+        shake.update(message)
+        # Output pseudorandom bytes and map them to coefficients.
+        hashed = [0 for i in range(n)]
+        i = 0
+        j = 0
+        while i < n:
+            # Takes 2 bytes, transform them in a 16 bits integer
+            twobytes = shake.read(2)
+            elt = (twobytes[0] << 8) + twobytes[1]  # This breaks in Python 2.x
+            # Implicit rejection sampling
+            if elt < k * q:
+                hashed[i] = elt % q
+                i += 1
+            j += 1
+        return hashed
+
+    def verify(self, message, signature):
+        """
+        Verify a signature.
+        """
+        # Unpack the salt and the short polynomial s1
+        salt = signature[HEAD_LEN : HEAD_LEN + SALT_LEN]
+        enc_s = signature[HEAD_LEN + SALT_LEN :]
+        s1 = decompress(enc_s, self.sig_bytelen - HEAD_LEN - SALT_LEN, self.n)
+        # Check that the encoding is valid
+        if s1 is False:
+            print("Invalid encoding")
+            return False
+        # Compute s0 and normalize its coefficients in (-q/2, q/2]
+        hashed = self.hash_to_point(message, salt)
+        s0 = sub_zq(hashed, mul_zq(s1, self.h))
+        s0 = [(coef + (q >> 1)) % q - (q >> 1) for coef in s0]
+        # Check that the (s0, s1) is short
+        norm_sign = sum(coef**2 for coef in s0)
+        norm_sign += sum(coef**2 for coef in s1)
+        if norm_sign > self.signature_bound:
+            print("Squared norm of signature is too large:", norm_sign)
+            return False
+
+        # If all checks are passed, accept
+        return True
 
     def __repr__(self):
         """Print the object in readable form."""
